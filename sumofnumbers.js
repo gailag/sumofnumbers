e447f4472e1447aa2389bf9c818b4351370aeacc
Gaila Galano <gailag@hawaii.edu>
2022-02-06 22:22:46 -1000

    E26

---
+++ sumofnumbers.js
@@ -0,0 +1,38 @@
+const int = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(int));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(int));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(int));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(int));
